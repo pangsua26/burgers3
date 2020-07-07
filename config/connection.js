962dd0a10556515cc54239e20909c5fd8c5caec4
Pangsua Vang <pangsua26@yahoo.com>
2020-07-07 12:52:57 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@
 // mysql connection
 const mysql = require("mysql");
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+};
 
 // make connection
 connection.connect(function(err) {
